516118b63191cc777134c176156712a312577adf
bryangraneros <65724327+bryangraneros@users.noreply.github.com>
2020-11-16 22:35:49 -0300

    Eliminado console.log innecesario

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Archivo js del lado del cliente cargado')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
